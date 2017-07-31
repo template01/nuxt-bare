2f9df80b67d16b16bcb1200901b2663825ed8b6e
Lasse <contact@template01.info>
2017-07-31 10:45:30 +0200

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,18 +21,5 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
   }
 }
